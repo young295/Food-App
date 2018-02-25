6aea0834fa023533806727ff84437f1159234e6d
Jesse Young <jgyoung295@gmail.com>
2018-02-24 22:13:05 -0500

    implement check for edit mode or create new recipe mode

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeEditComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  editMode = false;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.editMode = params['id'] != null; // if there is no id param present, then the mode is creating a new recipe, and not edit.
+      }
+    );
   }
 
 }
